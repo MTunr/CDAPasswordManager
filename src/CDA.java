f02dc796ce8985a69bac99d2702a830ccb670f7d
jchng <jarretchng02@gmail.com>
2019-12-02 13:33:42 +1100

    Add hashing algorithm for keys

--- src/CDA.java
+++ src/CDA.java
@@ -8,41 +8,45 @@
  */
 import java.lang.*;
 import java.lang.Math;
+import java.math.BigInteger;
 
 public class CDA {
     private static final int DEFAULT_SEED = 68867;
-    private static final int DEFAULT_KEY_HASH_LENGTH = 50;
+    private static final int DEFAULT_KEY_HASH_LENGTH = 10;
     private int seed;
 
     /**
      * Constructor for the CDA algorithm.
+     *
      * @param seed Seed for the CDA instance
      */
-    public CDA(int seed){
+    public CDA(int seed) {
         this.seed = seed;
     }
 
     /**
      * Default constructor for when a seed does not exist.
      */
-    public CDA(){
+    public CDA() {
         this.seed = DEFAULT_SEED;
     }
 
     /**
      * Encrypts the plaintext
+     *
      * @param plainText The given plaintext as a string
-     * @param key The key to use to encrypt the plaintext
+     * @param key       The key to use to encrypt the plaintext
      * @return The encrypted plaintext as a string
      */
-    public String encrypt(String plainText, String key){
+    public String encrypt(String plainText, String key) {
         return plainText;
     }
 
     /**
      * Decrypts the cipher text
+     *
      * @param cipherText The given cipher text as a string
-     * @param key The key used to encrypt the plaintext
+     * @param key        The key used to encrypt the plaintext
      * @return The decrypted cipher text as a string
      */
     public String decrypt(String cipherText, String key) {
@@ -51,20 +55,50 @@ public class CDA {
 
     /**
      * This class expands a given key into a fixed length string.
+     *
      * @return an Integer of a fixed length.
      */
-    public int keyExpander(String key){
-        int hash = 0;
+    public BigInteger keyExpander(String key) {
+        BigInteger hash = BigInteger.valueOf(1);
 
-        while (key.length() < DEFAULT_KEY_HASH_LENGTH){
-            key += key;
+        int oddCounter = 0;
+        while (key.length() < DEFAULT_KEY_HASH_LENGTH) {
+            key += (oddCounter % 2 == 0) ? reverse(key) : key;
+            oddCounter++;
         }
 
-        for (int i = 0; i < DEFAULT_KEY_HASH_LENGTH; i++){
-            hash = hash*13 + key.charAt(i);
+        for (int i = 0; i < key.length(); i++) {
+            hash = hash.add(BigInteger.valueOf(key.charAt(i)).pow(i+1));
+//            System.out.println(hash);
         }
 
         return hash;
+//        return shortenBigInteger(hash, DEFAULT_KEY_HASH_LENGTH);
     }
 
+    /**
+     * Takes in an integer and takes the last n "characters" of it
+     *
+     * @param integer The original BigInteger
+     * @param length  How many characters to take
+     * @return The shortened integer
+     */
+    public BigInteger shortenBigInteger(BigInteger integer, int length) {
+        return integer.mod(BigInteger.valueOf(10).pow(length));
+    }
+
+    /** TODO: Move to a string manipulation class
+     * Reverses a given string
+     * @param aString The string to reverse
+     * @return THe reversed string
+     */
+    public String reverse(String aString) {
+        String reversed = "";
+
+        for (int i = 0; i < aString.length(); i++) {
+            reversed = aString.charAt(i) + reversed;
+        }
+
+        return reversed;
+    }
 }
--- src/CDADebugger.java
+++ src/CDADebugger.java
@@ -5,5 +5,16 @@ public class CDADebugger {
     public static void main(String[] args) {
         CDA e = new CDA();
         System.out.println(e.keyExpander("hello"));
+        System.out.println(e.keyExpander("hellp"));
+        System.out.println(e.keyExpander("gello"));
+        System.out.println(e.keyExpander("o"));
+        System.out.println(e.keyExpander("fairly long password i gues"));
+        System.out.println(e.keyExpander("fairly long password i guess"));
+
+        //803999654436288 - hellp
+        //803851191445120 - hello
+        //866474805345920 - gello
+        //
+//        System.out.println("helldddd".hashCode());
     }
 }
