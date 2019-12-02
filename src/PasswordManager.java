4ebf450876c19b37b66d2c62d27ed13347c74bb2
Bryan <bryan.varic@gmail.com>
2019-12-02 13:30:25 +1100

    Created template for main running class. empty functionality and ui generation methods added in App class. created SearchBar class

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,5 +1,10 @@
+import uicomponents.SearchBar;
 import javax.swing.*;
+import java.awt.*;
 
+/**
+ * Main class
+ */
 public class PasswordManager{
     public static void main(String[] args) {
         App passwordManagerApp = new App();
@@ -20,9 +25,31 @@ class App implements Runnable{
         final int WINDOW_WIDTH = 500;
         // Create new empty window
         MainFrame appWindow = new MainFrame(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setMainScreen(appWindow);
+    }
+
+    // Functionality methods
+    void login(){}
+    void logout(){}
+    void addAccount(){}
+    void deleteAccount(){}
+    void confirmDelete(){}
+
+    // UI generation methods
+    void setLoginScreen(){}
+    void popupDeleteWindow(){}
+    void popupAddWindow(){}
+    void popupConfirmWindow(){}
+
+    //TODO: refactor, recode and basically re-write this entire method using uicomponents.
+    void setMainScreen(MainFrame appWindow){
+        appWindow.setLayout(new BorderLayout());
+        SearchBar mainSearchBar = new SearchBar();
+        appWindow.add(mainSearchBar, BorderLayout.PAGE_START);
     }
 }
 
+//TODO: move this to uicomponents.
 /**
  * Default frame class to generate windows.
  */
@@ -37,5 +64,4 @@ class MainFrame extends JFrame{
         setSize(windowWidth, windowHeight);
         setVisible(true);
     }
-
 }
