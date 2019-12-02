345348a938512931808a0b438d6447e04052af1c
jchng <jarretchng02@gmail.com>
2019-12-02 11:36:41 +1100

    Add Debugger for CDA.

--- src/CDA.java
+++ src/CDA.java
@@ -1,12 +1,14 @@
-/**
+/*
+ * Date: 2/12/2019
  * This class implements the CDA algorithm
  * CDA is a very slow string only encryption algorithm
  * that takes in a string and returns another string
  *
- * @author Jheng Ch'ng
- *
- * Date: 2/12/2019
+ * Author: Jheng Jheng Ch'ng
  */
+import java.lang.*;
+import java.lang.Math;
+
 public class CDA {
     private static final int DEFAULT_SEED = 68867;
     private static final int DEFAULT_KEY_HASH_LENGTH = 50;
@@ -51,8 +53,18 @@ public class CDA {
      * This class expands a given key into a fixed length string.
      * @return an Integer of a fixed length.
      */
-    public int keyExpander(){
-        return 0;
+    public int keyExpander(String key){
+        int hash = 0;
+
+        while (key.length() < DEFAULT_KEY_HASH_LENGTH){
+            key += key;
+        }
+
+        for (int i = 0; i < DEFAULT_KEY_HASH_LENGTH; i++){
+            hash = hash*13 + key.charAt(i);
+        }
+
+        return hash;
     }
 
 }
---
+++ src/CDADebugger.java
@@ -0,0 +1,9 @@
+/**
+ * Debugging class for CDA
+ */
+public class CDADebugger {
+    public static void main(String[] args) {
+        CDA e = new CDA();
+        System.out.println(e.keyExpander("hello"));
+    }
+}
